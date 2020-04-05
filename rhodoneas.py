f97ef77d0e5f82a90e7dc83f7504189d57c9eba0
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-04-05 19:37:42 +0200

    Add files via upload

---
+++ rhodoneas.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+import time
+
+iterations=1000
+final = False
+fig = plt.figure()
+ax = plt.axes(xlim=(-1,1), ylim=(-1,1))
+rose, = ax.plot([],[],'r')
+
+def init():
+	rose.set_data([],[])
+	return rose,
+
+
+def animate(i):
+	global final
+	num = 5/8
+	thetas = np.linspace(0, (2*np.pi)*8, iterations)
+	x = np.cos(num*thetas)*np.cos(thetas)
+	y = np.cos(num*thetas)*np.sin(thetas)
+	rose.set_data(x[0:i], y[0:i])
+	if i == 0 and final:
+		final = False
+		time.sleep(10)
+	elif i==0 and not final:
+		final = True
+	return rose,
+
+
+animation.FuncAnimation(fig, animate, init_func=init, frames=iterations, interval=10, blit=True)
+plt.show()
